7b4d4a5c92a049aa535b70864713cdd1b226afc6
Muhammad Faisal <muhfaisal90@hotmail.com>
2020-08-22 10:40:08 +0500

    First Commit

---
+++ precache-manifest.dd600ba94ed78e6edd4c691ca43d8491.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76610d6c1bab957fa822d7719ed75c23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e10bd42ba5eea1c601b9",
+    "url": "/static/css/main.12a168a8.chunk.css"
+  },
+  {
+    "revision": "298ee884f71959504303",
+    "url": "/static/js/2.c8aebb64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c8aebb64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10bd42ba5eea1c601b9",
+    "url": "/static/js/main.cc1b6285.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
